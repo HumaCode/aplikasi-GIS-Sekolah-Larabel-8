db1733722b8755e195ebbe891ae2231c4e0ec9dd
HumaCode <humaidizakaria1998@gmail.com>
2021-12-30 20:55:08 +0700

    update2

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -195,7 +195,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <footer class="main-footer">
         <!-- To the right -->
         <div class="float-right d-none d-sm-inline">
-            Github <a href="">Klik Disini</a>
+            Github <a href="https://github.com/HumaCode/aplikasi-GIS-Sekolah-Larabel-8">Klik Disini</a>
         </div>
         <!-- Default to the left -->
         <strong>Copyright &copy; 2021.</strong> Humaidi Zakaria.
